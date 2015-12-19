312ecc071db2ee278956843471bb5a6065e6aa26
truongngoctuan <tntuan0712494@gmail.com>
2015-12-19 22:32:44 +0700

    add Agencies controller/views

--- wmWebApp/wmWebApp/Models/Agency.cs
+++ wmWebApp/wmWebApp/Models/Agency.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,13 @@ namespace wmWebApp.Models
     public class Agency
     {
         public int Id { get; set; }
+        [Display(Name = "Tên Chi Nhánh")]
         public string Name { get; set; }
+        [Display(Name = "Chi Tiết")]
         public string Desc { get; set; }
+        [Display(Name = "Thứ tụ hiển thị")]
         public uint Ranking { get; set; }
+        [Display(Name = "Loại Chi Nhánh")]
         public AgencyType Type { get; set; }
     }
 }
