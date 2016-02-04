7e97d677c928fe4acce0138a6c7cb7ce6ef66072
truongngoctuan <tntuan0712494@gmail.com>
2016-02-04 21:21:52 +0700

    authorize dashboard

---
+++ wmWebApp/wm.Web2/Controllers/DashboardController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wm.Web2.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        [Authorize]
+        public ActionResult GeneralIndex()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult StaffIndex()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult AdminIndex()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult BranchManagerIndex()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult StockIndex()
+        {
+            return View();
+        }
+    }
+}
