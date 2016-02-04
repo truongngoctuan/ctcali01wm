ca00a6972f26638795b4c8c2b9feff524a4e55d0
truongngoctuan <tntuan0712494@gmail.com>
2016-02-04 20:35:42 +0700

    missing file

---
+++ wmWebApp/wm.Web2/Models/OrderViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using wm.Service.Model;
+
+namespace wm.Web2.Models
+{
+    public class OrderViewModel
+    {
+        public OrderBranchItem[] data { get; set; }
+    }
+}
