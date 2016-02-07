5a4999c663bf0f1210e8194abb74b10a9ccdabba
truongngoctuan <tntuan0712494@gmail.com>
2016-02-07 11:56:13 +0700

    temp commit

---
+++ wmWebApp/wm.Model/SystemRoles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wm.Model
+{
+    public static class SystemRoles
+    {
+        public static string SuperUser { get { return "superman"; } }
+        public static string Admin { get { return "admin"; } }
+        public static string Staff { get { return "staff"; } }
+        public static string Manager { get { return "manager"; } }
+        public static string WarehouseKeeper { get { return "whkeeper"; } }
+    }
+}
--- wmWebApp/wm.Web2/Migrations/Configuration.cs
+++ wmWebApp/wm.Web2/Migrations/Configuration.cs
@@ -1,10 +1,19 @@
 namespace wm.Web2.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Model;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-
+    using System.Web;
+    using System.Web.Mvc;
+    using Microsoft.AspNet.Identity.Owin;
+    using Microsoft.Owin.Security;
+    using Microsoft.Owin.Host.SystemWeb;
+    using System.Net.Http;
     internal sealed class Configuration : DbMigrationsConfiguration<Model.wmContext>
     {
         public Configuration()
@@ -13,6 +22,62 @@ namespace wm.Web2.Migrations
             AutomaticMigrationDataLossAllowed = true;
         }
 
+        protected void AddRoleIfNotExist(string roleName)
+        {
+            var rm = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+
+            if (!rm.RoleExists(roleName))
+            {
+                var roleResult = rm.Create(new IdentityRole(roleName));
+                if (!roleResult.Succeeded)
+                    throw new ApplicationException("Creating role " + roleName + "failed with error(s): " + roleResult.Errors);
+            }
+
+        }
+
+        protected void CreateUser(Model.wmContext context, RegisterViewModel model)
+        {
+            var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+            ApplicationUserManager userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+
+            var existingUser = userManager.FindByName(model.UserName);
+            if (existingUser != null)
+            {
+                userManager.RemovePassword(existingUser.Id);
+                userManager.AddPassword(existingUser.Id, model.PlainPassword);
+
+                var existingEmployee = context.Employees.Where(s => s.Id == existingUser.Id).First();
+
+                existingEmployee.PlainPassword = model.PlainPassword;
+                existingEmployee.Id = user.Id;
+                existingEmployee.UserName = user.UserName;
+                existingEmployee.Name = model.FullName;
+                existingEmployee.BranchId = model.BranchId;
+                existingEmployee.Role = model.Role;
+            }
+            else
+            {
+                var result = userManager.Create(user, model.PlainPassword);
+
+                if (result.Succeeded)
+                {
+                    var employee = new Employee
+                    {
+                        PlainPassword = model.PlainPassword, //when user change password, this will be reset
+
+                        Id = user.Id,
+                        UserName = user.UserName,
+                        Name = model.FullName,
+                        BranchId = model.BranchId,
+                        Role = model.Role
+                    };
+                    context.Employees.AddOrUpdate(employee);
+                    context.SaveChanges();
+                }
+            }
+
+
+        }
         protected override void Seed(Model.wmContext context)
         {
             //  This method will be called after migrating to the latest version.
@@ -27,6 +92,9 @@ namespace wm.Web2.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+
+
             context.Branches.AddOrUpdate(new Model.Branch { Id = 1, Name = "branch 1" });
             context.Branches.AddOrUpdate(new Model.Branch { Id = 2, Name = "branch 2" });
             context.Branches.AddOrUpdate(new Model.Branch { Id = 3, Name = "branch 3" });
@@ -52,7 +120,7 @@ namespace wm.Web2.Migrations
             context.GoodUnits.AddOrUpdate(new Model.GoodUnit { Id = 3, Name = "unit 3" });
             context.SaveChanges();
 
-            context.Goods.AddOrUpdate(new Model.Good { Id = 1, Name = "good 1", UnitId = 1});
+            context.Goods.AddOrUpdate(new Model.Good { Id = 1, Name = "good 1", UnitId = 1 });
             context.Goods.AddOrUpdate(new Model.Good { Id = 2, Name = "good 2", UnitId = 2 });
             context.Goods.AddOrUpdate(new Model.Good { Id = 3, Name = "good 3", UnitId = 1 });
             context.Goods.AddOrUpdate(new Model.Good { Id = 4, Name = "good 4", UnitId = 3 });
@@ -75,6 +143,18 @@ namespace wm.Web2.Migrations
             context.Orders.AddOrUpdate(new Model.Order { Id = 1, BranchId = 1, CreatedDate = DateTime.UtcNow, OrderDay = DateTime.UtcNow, UpdatedDate = DateTime.UtcNow, Status = Model.OrderStatus.Started });
             context.Orders.AddOrUpdate(new Model.Order { Id = 2, BranchId = 2, CreatedDate = DateTime.UtcNow, OrderDay = DateTime.UtcNow, UpdatedDate = DateTime.UtcNow, Status = Model.OrderStatus.Started });
             context.SaveChanges();
+
+
+            #region roles and pre define users
+            AddRoleIfNotExist(SystemRoles.SuperUser);
+            AddRoleIfNotExist(SystemRoles.Admin);
+            AddRoleIfNotExist(SystemRoles.WarehouseKeeper);
+            AddRoleIfNotExist(SystemRoles.Manager);
+            AddRoleIfNotExist(SystemRoles.Staff);
+
+            CreateUser(context, new RegisterViewModel { UserName = "admin", Email = "tntuan0712494@gmail.com", FullName = "TNT", BranchId = 1, PlainPassword = "asdasd", Role = EmployeeRole.Admin });
+            #endregion
+
         }
     }
 }
