ad92710269c8f17ff85ba34cae81cfb0cc14039c
truongngoctuan <tntuan0712494@gmail.com>
2016-02-21 16:07:26 +0700

    cleanup code

---
+++ wmWebApp/wm.Web2/Content/EditOrderHelper.js
@@ -0,0 +1,15 @@
+﻿function clickGoodCategoryItem(url) {
+    if (window.isDataChanged) {
+        //confirm before change page
+        bootbox.confirm("There are unsave data, do you want to leave this page ? ", function (result) {
+            if (result) {
+                console.log("still want to leave this page");
+                window.location.href = url;
+            }
+        });
+
+
+    } else {
+        window.location.href = url;
+    }
+}
