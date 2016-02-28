ce22858ad0cbe98096e1fc7d73a60957dd403641
truongngoctuan <tntuan0712494@gmail.com>
2016-02-29 00:09:50 +0700

    localize for login

--- wmWebApp/wm.Web2/Views/hhs.cs
+++ wmWebApp/wm.Web2/Views/hhs.cs
@@ -5,6 +5,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,14 +14,46 @@ namespace System.Web
 {
     public static class hhs
     {
+        private static ResourceManager InitResources(ResourceManager resourceManager, string filename = "BasicResources")
+        {
+            if (resourceManager == null)
+            {
+                Assembly assembly = System.Reflection.Assembly.Load("App_GlobalResources");
+                resourceManager = new ResourceManager("Resources." + filename, assembly);
+            }
+
+            return resourceManager;
+        }
+
+        public static ResourceManager resourceManager = null;
         //http://stackoverflow.com/a/273971/3161505
         public static string GetName<T, U>(Expression<Func<T, U>> expression)
         {
             var member = expression.Body as MemberExpression;
             if (member != null)
+            {
                 return member.Member.Name;
+            }
 
             throw new ArgumentException("Expression is not a member access", "expression");
+        }
+
+        public static string GetTranslatedName<T, U>(Expression<Func<T, U>> expression)
+        {
+            var member = expression.Body as MemberExpression;
+            if (member != null)
+            {
+                return hhs.GetTranslatedName(member.Member.Name);
+            }
+
+            throw new ArgumentException("Expression is not a member access", "expression");
+        }
+
+        public static string GetTranslatedName(string input)
+        {
+            resourceManager = hhs.InitResources(resourceManager);
+            string result = resourceManager.GetString(input);
+            return (result == null || result == String.Empty) ? input : result;
         }
     }
 }
